2d36cf3873df62308afdfd167faa54c0c7c3b31f
Eduardo_Tavares <48965705+eduardotavares509@users.noreply.github.com>
2021-02-16 00:47:30 -0300

    ssl configuration test 5

--- config/database.js
+++ config/database.js
@@ -80,13 +80,14 @@ module.exports = {
       user: Env.get("DB_USER", "root"),
       password: Env.get("DB_PASSWORD", ""),
       database: Env.get("DB_DATABASE", "adonis"),
-    },
-    ssl: true,
-    extra: {
-      ssl: {
-        rejectUnauthorized: false,
+      ssl: true,
+      extra: {
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
     },
+
     debug: Env.get("DB_DEBUG", false),
   },
 };
